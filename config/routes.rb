172de6bbdc2a15849e9c37532cf49cf88c92a4e4
Suzu Nanri <2111140058t@ed.fuk.kindai.ac.jp>
2023-10-28 14:32:52 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
+  get 'tops/main'
+  get 'tops/login'
+  get 'tops/logout'
+  post 'top/login'
+  get 'top/logout'
+  
+  get 'tweets/index'
+  get 'tweets/new'
+  post 'tweets/create'
+  get 'tweets/destroy'
+  #resources :tweets
+  root 'tweets#index'
+  delete 'tweets/:id' ,to: 'tweets#destroy'
+  
   get 'users/index'
   get 'users/new'
   post 'users/create'
   post 'users/destroy'
   #resources :users #これを書いていればOKのはず？なぜか機能していない
-  root 'users#index'
+  #root 'users#index'
   #get 'users/:id' ,to: 'users#destroy'
   delete 'users/:id' ,to: 'users#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
